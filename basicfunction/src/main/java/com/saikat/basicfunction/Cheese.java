6ecfbd2861cf693eed29b0d90505963e42d94b38
Fojle Rabbi Saikat <saikataiub29@gmail.com>
2019-11-05 21:38:56 +0600

    Hide and Show View Added

--- basicfunction/src/main/java/com/saikat/basicfunction/Cheese.java
+++ basicfunction/src/main/java/com/saikat/basicfunction/Cheese.java
@@ -1,18 +1,42 @@
 package com.saikat.basicfunction;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.Toast;
 
 public class Cheese {
 
     Context context;
 
+    /*
+    * CONSTRUCTOR INITIATED
+    * @param
+    *  - context
+    * */
     public Cheese(Context context){
         this.context = context;
     }
 
+    /*
+    * SHOW TOAST
+    * @Param
+    *  - string
+    * */
     public void showToast(String message){
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    /*
+    * HIDE / SHOW VISIBILITY
+    * @param
+    *  - array of views [views to hide]
+    *  - view [view to show]
+    * */
+    public void hideAndShowViews(View[] views, View view) {
+        for (int i = 0; i < views.length; i++) {
+            views[i].setVisibility(View.GONE);
+        }
+        view.setVisibility(View.VISIBLE);
     }
 
 }
